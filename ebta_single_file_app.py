1f6cbae92a6f6ca20691c2c03d68b0ca44fa9dfa
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2026-03-07 12:54:25 +0200

    Add files via upload

--- ebta_single_file_app.py
+++ ebta_single_file_app.py
@@ -12418,7 +12418,5 @@ def payfast_ipn():
 
 
 # ===================== MAIN ==============
-if __name__ == '__main__':
-    init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
